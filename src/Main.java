df2b3c716ce8d1354cabe3e54e9062bc15fdd294
Dan Marks <83971193+dan76-20@users.noreply.github.com>
2021-05-17 08:36:02 -0600

    Added greeting for Daniel

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
         System.out.println("Hello, my name is Hector");
         System.out.println("Hello Hector, I am Fernando");
+        System.out.println("Hello Hector, I am Daniel.");
     }
 }
